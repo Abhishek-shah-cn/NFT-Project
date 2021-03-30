b2a687b3d472f95e29ac786b297dfef007bc845f
Abhishek shah <54413214+Abhishek-shah-coding-ninja@users.noreply.github.com>
2021-03-30 06:44:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import Contain from "./Contain";
 import Navbar from "./Navbar";
-
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Contain />
+      <Footer/>
     </div>
   );
 }
